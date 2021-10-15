8e8c042ed203fafe136e5e38e2cffecf8efe6931
IgnatevArtem <ignatev.artem.nik@yandex.ru>
2021-10-15 17:10:37 +0400

    Роутинг для меню администратора

--- Shop/src/app/app.module.ts
+++ Shop/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { MainPageComponent } from './main-page/main-page.component';
 import { ProductPageComponent } from './product-page/product-page.component';
 import { CartPageComponent } from './cart-page/cart-page.component';
 
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -18,7 +19,8 @@ import { CartPageComponent } from './cart-page/cart-page.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
